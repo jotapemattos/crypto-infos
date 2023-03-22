84b3a25b24c6eb946116a72fdf905e19813a8c21
jotapemattos <jpmrc49@gmail.com>
2023-03-22 17:32:04 -0300

    add a currency select

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -1,7 +1,12 @@
 import CryptoLogo from '/crypto-logo.svg'
 import HamburgerIcon from './HamburgerIcon'
+import { useContext } from 'react'
+import { CurrencyContext } from '../../context/Currency'
+
 
 const NavBar = () => {
+  const { currency, setCurrency} = useContext(CurrencyContext)
+
   return (
     <div className='w-full h-24 flex items-center md:items-end justify-center p-2 md:p-0 absolute z-30 inset-0'>
         <div className='w-full md:w-4/5 lg:w-4/5 xl:w-2/3 h-16 flex items-center justify-between p-4 text-white bg-white/10 bg-opacity-10 backdrop-blur-sm rounded-full drop-shadow-lg border border-white/20'>
@@ -14,7 +19,16 @@ const NavBar = () => {
                 <a href='' className='hover:cursor-pointer'>Exchanges</a>
                 <a href='' className='hover:cursor-pointer'>Markets</a>
             </nav>
-            <a href='' className='text-base w-1/3 hidden lg:flex justify-end hover:cursor-pointer'>News</a>
+            <div className='text-base w-1/3 hidden lg:flex justify-end '>
+              <select 
+                className='bg-transparent outline-none hover:cursor-pointer'
+                value={currency}
+                onChange={(e) => setCurrency(e.target.value)}
+              >
+                <option className='p-2 bg-[#020017]' value="INR">₹INR</option>
+                <option className='p-2 bg-[#020017]' value="USD">$USD</option>
+              </select>
+            </div>
             <HamburgerIcon/>
         </div>
     </div>

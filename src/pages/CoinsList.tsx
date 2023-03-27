663eadaa743b461e209c8e013b5740b93f4c6951
jotapemattos <jpmrc49@gmail.com>
2023-03-27 15:52:59 -0300

    build the coins page

--- src/pages/CoinsList.tsx
+++ src/pages/CoinsList.tsx
@@ -21,11 +21,11 @@ const CoinsList = () => {
     .then((response) => setData(response.data))
   }, [currency])
   return (
-    <div className='flex flex-col items-center justify-center pb-20 min-w-screen min-h-full'>
+    <div className='coinsList flex flex-col items-center justify-center pb-20 min-w-screen min-h-full'>
         <div className='w-5/6 flex flex-col gap-10 h-auto mt-40'>
             {data.map((coin) => (
-                <div className='w-full flex items-center justify-between bg-blue-900/30 rounded-md'>
-                    <div className='w-1/2 h-28 p-4 flex items-center justify-start gap-4 text-white'>
+                <div className='w-full flex items-center justify-between bg-blue-900/20 rounded-2xl shadow-xl'>
+                    <div className='w-1/3 h-28 p-4 flex items-center justify-start gap-4 text-white'>
                         <img src={coin.image} alt="coin-image" className='h-full'/>
                         <p className='text-2xl'>{coin.name}</p>
                         <div className='text-2xl flex items-center justify-center gap-2'>
@@ -33,7 +33,7 @@ const CoinsList = () => {
                             <span>{coin.current_price.toLocaleString(currency)}</span>
                         </div>
                     </div>
-                    <div className='w-1/2 h-28 p-4 flex items-center justify-end gap-4 text-white'>
+                    <div className='w-1/3 h-28 p-4 flex items-center justify-center gap-4 text-white'>
                         <p className='text-2xl'>{coin.symbol.toUpperCase()}</p>
                         <div className=" flex justify-center gap-4">
                             {coin.price_change_percentage_24h >= 0? 
@@ -44,6 +44,9 @@ const CoinsList = () => {
                                 {coin.price_change_percentage_24h.toFixed(2)}%
                             </span>)}
                         </div>
+                    </div>
+                    <div className='w-1/3 h-28 p-4 flex items-center justify-end gap-4 text-white'>
+                        <p>src/pages/CoinsList.tsx{coin.market_cap_rank}</p>
                     </div>
                 </div>
             ))}

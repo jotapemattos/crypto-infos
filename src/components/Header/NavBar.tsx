a99fd81b13d5929a25eebc3a4a66732d24e6cc21
jotapemattos <jpmrc49@gmail.com>
2023-03-27 15:52:28 -0300

    add the currency select

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -11,18 +11,16 @@ const NavBar = () => {
   return (
     <div className='w-full h-24 flex items-center md:items-end justify-center p-2 md:p-0 absolute z-30 inset-0'>
         <div className='w-full md:w-4/5 lg:w-4/5 xl:w-2/3 h-16 flex items-center justify-between p-4 text-white bg-white/10 bg-opacity-10 backdrop-blur-sm rounded-full drop-shadow-lg border border-white/20'>
-            <div className='w-1/3 flex items-center justify-start gap-2 text-2xl hover:cursor-pointer'>
-                <Link to={'/'}>
-                  <img src={CryptoLogo} alt="crypto-logo" className='w-12'/>
-                </Link>
-                <p>Crypto Corner</p>
-            </div>
-            <nav className='hidden lg:flex items-center justify-around w-1/3 text-base'>
+          <Link to={'/'} className='w-1/3 flex items-center justify-start gap-2 text-2xl hover:cursor-pointer'>
+            <img src={CryptoLogo} alt="crypto-logo" className='w-12'/>
+            <p>Crypto Corner</p>
+          </Link>
+          <nav className='hidden lg:flex items-center justify-around w-1/3 text-base'>
                 <Link to={'/coinslist'}>
-                  <h4 className='hover:cursor-pointer'>Coins</h4>
+                  <h4 className='hover:text-blue-300 hover:cursor-pointer transition-all duration-300'>Coins</h4>
                 </Link>
-                <h4 className='hover:cursor-pointer'>Exchanges</h4>
-                <h4 className='hover:cursor-pointer'>Markets</h4>
+                <h4 className='hover:text-blue-300 hover:cursor-pointer transition-all duration-300'>Exchanges</h4>
+                <h4 className='hover:text-blue-300 hover:cursor-pointer transition-all duration-300'>Markets</h4>
             </nav>
             <div className='text-base w-1/3 hidden lg:flex justify-end '>
               <select 

473ace5eb7ff22683504e2da03270faa5f8377f0
jotapemattos <jpmrc49@gmail.com>
2023-03-31 16:24:23 -0300

    build the coins page

--- src/components/GlobalStats/CoinsCards.tsx
+++ src/components/GlobalStats/CoinsCards.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useContext, useEffect, useState } from "react"
 import { CurrencyContext } from "../../context/Currency"
-import  { TrendingCoins }  from '../../utils/api'
+import  { TrendingCoins }  from '../../services/api'
 
 interface TrendingCoinsTypes {
     current_price: number,
@@ -16,26 +16,49 @@ export const CoinsCards = () => {
   const [data, setData] = useState(Array<TrendingCoinsTypes>)
 
   useEffect(()=>{
-    axios.get(TrendingCoins(currency))
+    //axios cancellation token in case the user cancels the request!
+    const cancelToken = axios.CancelToken.source()
+    axios.get(TrendingCoins(currency), {cancelToken: cancelToken.token})
     .then((response)=> setData(response.data))
+        .catch((err) => {
+            if (axios.isCancel(err)){
+                console.log("request cancelled")
+            }
+            else {
+                //handleerror
+            }
+        })
+
+    return () => {
+        cancelToken.cancel()
+    }
   }, [currency])
 
   return (
     <div className="w-full grid grid-cols-5 gap-10 text-white">
         {data.map((coin) => (
-            <div className='w-60 h-auto flex flex-col justify-center items-center gap-4 p-2 bg-blue-900/30 backdrop-blur-sm rounded-lg shadow-2xl'>
+            <div 
+                key={coin.symbol} 
+                className='w-60 h-auto flex flex-col justify-center items-center gap-4 p-2 bg-blue-900/30 backdrop-blur-sm rounded-lg shadow-2xl'
+            >
                 <img src={coin.image} alt="coin-image" className="w-20"/>
                 <div className=" flex justify-center gap-4">
                     <span className="text-white">{coin.symbol.toUpperCase()}</span> 
-                    {coin.price_change_percentage_24h >= 0? 
-                    (<span className="text-green-500">
-                        {coin.price_change_percentage_24h.toFixed(2)}%
-                    </span>): 
-                    (<span className="text-red-700">
-                        {coin.price_change_percentage_24h.toFixed(2)}%
-                    </span>)}
+                    {coin.price_change_percentage_24h >= 0 && (
+                        <span className="text-green-500">
+                            {coin.price_change_percentage_24h.toFixed(2)}%
+                        </span>
+                    )}
+                    {coin.price_change_percentage_24h < 0 && (
+                        <span className="text-red-700">
+                            {coin.price_change_percentage_24h.toFixed(2)}%
+                        </span>
+                    )}
                 </div>
-                <div className="font-bold text-xl flex gap-2"><span>{symbol}</span><span>{coin.current_price.toLocaleString(currency)}</span></div>
+                <div className="font-bold text-xl flex gap-2">
+                    <span>{symbol}</span>
+                    <span>{coin.current_price.toLocaleString(currency)}</span>
+                </div>
             </div>
         ))}
     </div>
--- src/pages/CoinsList.tsx
+++ src/pages/CoinsList.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { CurrencyContext } from '../context/Currency'
-import { CoinList } from '../utils/api'
+import { CoinList } from '../services/api'
 
 interface CoinsType {
     name: string,
@@ -17,14 +17,31 @@ const CoinsList = () => {
   const [data, setData] = useState(Array<CoinsType>)  
 
   useEffect(() => {
-    axios.get(CoinList(currency))
+    //axios cancellation token in case the user cancels the request
+    const cancelToken = axios.CancelToken.source() 
+    axios.get(CoinList(currency), {cancelToken: cancelToken.token})
     .then((response) => setData(response.data))
+     .catch((err) => {
+        if (axios.isCancel(err)) {
+            console.log("request cancelled")
+        }
+        else {
+            //handleerror
+        }
+     })
+    
+    return () => {
+        cancelToken.cancel()
+    }
   }, [currency])
   return (
     <div className='coinsList flex flex-col items-center justify-center pb-20 min-w-screen min-h-full'>
         <div className='w-5/6 flex flex-col gap-10 h-auto mt-40'>
             {data.map((coin) => (
-                <div className='w-full flex items-center justify-between bg-blue-900/20 rounded-2xl shadow-xl'>
+                <div 
+                    key={coin.symbol} 
+                    className='w-full flex items-center justify-between bg-blue-900/20 rounded-2xl shadow-xl'
+                >
                     <div className='w-1/3 h-28 p-4 flex items-center justify-start gap-4 text-white'>
                         <img src={coin.image} alt="coin-image" className='h-full'/>
                         <p className='text-2xl'>{coin.name}</p>
@@ -36,17 +53,20 @@ const CoinsList = () => {
                     <div className='w-1/3 h-28 p-4 flex items-center justify-center gap-4 text-white'>
                         <p className='text-2xl'>{coin.symbol.toUpperCase()}</p>
                         <div className=" flex justify-center gap-4">
-                            {coin.price_change_percentage_24h >= 0? 
-                            (<span className="text-green-500">
-                                {coin.price_change_percentage_24h.toFixed(2)}%
-                            </span>): 
-                            (<span className="text-red-700">
-                                {coin.price_change_percentage_24h.toFixed(2)}%
-                            </span>)}
+                            {coin.price_change_percentage_24h >= 0 && (
+                                <span className="text-green-500">
+                                    {coin.price_change_percentage_24h.toFixed(2)}%
+                                </span>
+                            )}
+                            {coin.price_change_percentage_24h < 0 && (
+                                <span className="text-red-700">
+                                    {coin.price_change_percentage_24h.toFixed(2)}%
+                                </span>
+                            )}
                         </div>
                     </div>
                     <div className='w-1/3 h-28 p-4 flex items-center justify-end gap-4 text-white'>
-                        <p>src/pages/CoinsList.tsx{coin.market_cap_rank}</p>
+                        <p>#{coin.market_cap_rank}</p>
                     </div>
                 </div>
             ))}

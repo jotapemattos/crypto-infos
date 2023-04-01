b4d77a28276ed0ab80193f8fac5e165d033a14d8
jotapemattos <jpmrc49@gmail.com>
2023-03-31 22:55:23 -0300

    add context provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import CurrencyContextProvider from './context/Currency'
+import SearchQueryContextProvider from './context/SearchQuery'
+import CoinsContextProvider from './context/Coins'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CurrencyContextProvider>
-      <App />
-    </CurrencyContextProvider>
-  </React.StrictMode>,
+    <CoinsContextProvider>
+      <SearchQueryContextProvider>
+        <CurrencyContextProvider>
+          <App />
+        </CurrencyContextProvider>
+      </SearchQueryContextProvider>
+    </CoinsContextProvider>
+  </React.StrictMode>
 )

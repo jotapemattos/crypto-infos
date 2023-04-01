e32011448f35abf938b9a5f1b37dccaf39a4dab2
jotapemattos <jpmrc49@gmail.com>
2023-03-31 23:00:37 -0300

    add a @types directory

---
+++ src/@types/interfaces.ts
@@ -0,0 +1,8 @@
+export interface CoinsType {
+        name: string,
+        symbol: string,
+        image: string,
+        market_cap_rank: number,
+        current_price: number,
+        price_change_percentage_24h: number
+    }

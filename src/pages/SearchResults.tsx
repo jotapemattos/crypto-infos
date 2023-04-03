dd8cfc3d0009ad5dcf5b0cbf22f3776c585425e6
jotapemattos <jpmrc49@gmail.com>
2023-04-03 16:04:06 -0300

    built the search results page

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,8 +1,11 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { SearchQueryContext } from "../context/SearchQuery"
 import { useState } from "react"
 import axios from "axios"
 import { SearchCoin } from "../services/api"
+import { Link } from "react-router-dom"
+import { CurrencyContext } from "../context/Currency"
+
 
 interface SearchCoinsProps {
   api_symbol: string,
@@ -16,15 +19,50 @@ interface SearchCoinsProps {
 
 const SearchResults = () => {
   const { search } = useContext(SearchQueryContext)
+  const { currency } = useContext(CurrencyContext)
   const [ searchCoins, setSearchCoins] = useState(Array<SearchCoinsProps>)
 
   useEffect(() => {
     axios.get(SearchCoin(search.toLowerCase()))
     .then((response) => setSearchCoins(response.data.coins))
-  }, [])
+    .catch((err) => console.log(err))
+  }, [currency])
 
   return (
-    <div className="text-white">{searchCoins.map((coin) => (coin.id))}</div>
+    <div className="results w-full flex flex-col items-center gap-10 pb-20">
+      {searchCoins.length > 0 && (
+        <div className="flex flex-col items-center gap-8">
+          <h1 className="text-white text-3xl text-center pt-36">Results for "{search}"</h1>
+          <Link to={'/'}>
+            <button className="text-white border border-white/20 py-2 px-4 hover:bg-white/20 transition-colors duration-300">Back to search</button>
+          </Link>
+      </div>
+      )}
+      {searchCoins.length === 0 && (
+        <div className="flex flex-col items-center gap-8">
+          <h1 className="text-white text-3xl text-center pt-36">No results for "{search}"</h1>
+          <Link to={'/'}>
+            <button className="text-white border border-white/20 py-2 px-4 hover:bg-white/20 transition-colors duration-300">Back to search</button>
+          </Link>
+        </div>
+      )}
+      <main 
+        className="w-4/5 grid grid-cols-5 gap-10 items-center justify-items-center">
+        {searchCoins.map((coin) => (
+          <div 
+            className="relative w-60 h-64 flex flex-col justify-center items-center gap-4 p-2 text-white bg-gradient-to-br from-white/10 to-white/0 backdrop-blur-sm rounded-xl drop-shadow-lg border border-white/20"
+            key={coin.id}
+          >
+            <h1>#{coin.market_cap_rank}</h1>
+            <img src={coin.large} alt="coin-image" className="w-20"/>
+            <div className="flex flex-col items-center text-center">
+              <h1>{coin.name}</h1>
+              <h2>{coin.symbol}</h2>
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
   )
 }
 

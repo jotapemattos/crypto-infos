8efb7e1b721ba00e08af48ca34b5cc298659fe3c
jotapemattos <jpmrc49@gmail.com>
2023-04-04 13:07:49 -0300

    add a loader in api calls

--- src/components/GlobalStats/CoinsCards.tsx
+++ src/components/GlobalStats/CoinsCards.tsx
@@ -1,66 +1,69 @@
 import axios from "axios"
 import { useContext, useEffect, useState } from "react"
 import { CurrencyContext } from "../../context/Currency"
-import  { TrendingCoins }  from '../../services/api'
+import { TrendingCoins } from '../../services/api'
+import Loader from "../Loader"
 
 interface TrendingCoinsTypes {
-    current_price: number,
-    image: string,
-    name: string,
-    symbol: string,
-    price_change_percentage_24h: number
+  current_price: number,
+  image: string,
+  name: string,
+  symbol: string,
+  price_change_percentage_24h: number
 }
 
 export const CoinsCards = () => {
   const { currency, symbol } = useContext(CurrencyContext)
   const [data, setData] = useState(Array<TrendingCoinsTypes>)
+  const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(()=>{
-    //axios cancellation token in case the user cancels the request!
-    const cancelToken = axios.CancelToken.source()
-    axios.get(TrendingCoins(currency), {cancelToken: cancelToken.token})
-    .then((response)=> setData(response.data))
-        .catch((err) => {
-            if (axios.isCancel(err)){
-                console.log("request cancelled")
-            }
-            else {
-                //handleerror
-            }
-        })
+  const fetchTrendingCoins = async () => {
+    await axios.get(TrendingCoins(currency))
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err.name))
+    setIsLoading(false)
+  }
 
-    return () => {
-        cancelToken.cancel()
-    }
+  useEffect(() => {
+    fetchTrendingCoins()
   }, [currency])
 
   return (
-    <div className="w-full grid grid-cols-5 gap-10 text-white">
+    <>
+      {isLoading && (
+        <div className="w-full h-full flex items-center justify-center">
+          <Loader/>
+        </div>
+      )}
+      {!isLoading && (
+        <div className="w-full grid grid-cols-5 gap-10 text-white">
         {data.map((coin) => (
-            <div 
-                key={coin.symbol} 
-                className='w-60 h-auto flex flex-col justify-center items-center gap-4 p-2 bg-gradient-to-br from-white/10 to-white/~5 backdrop-blur-sm rounded-xl drop-shadow-lg border border-white/20'
-            >
-                <img src={coin.image} alt="coin-image" className="w-20"/>
-                <div className=" flex justify-center gap-4">
-                    <span className="text-white">{coin.symbol.toUpperCase()}</span> 
-                    {coin.price_change_percentage_24h >= 0 && (
-                        <span className="text-green-500">
-                            {coin.price_change_percentage_24h.toFixed(2)}%
-                        </span>
-                    )}
-                    {coin.price_change_percentage_24h < 0 && (
-                        <span className="text-red-700">
-                            {coin.price_change_percentage_24h.toFixed(2)}%
-                        </span>
-                    )}
-                </div>
-                <div className="font-bold text-xl flex gap-2">
-                    <span>{symbol}</span>
-                    <span>{coin.current_price.toLocaleString(currency)}</span>
-                </div>
+          <div
+            key={coin.symbol}
+            className='w-60 h-auto flex flex-col justify-center items-center gap-4 p-2 bg-gradient-to-br from-white/10 to-white/~5 backdrop-blur-sm rounded-xl drop-shadow-lg border border-white/20'
+          >
+            <img src={coin.image} alt="coin-image" className="w-20" />
+            <div className=" flex justify-center gap-4">
+              <span className="text-white">{coin.symbol.toUpperCase()}</span>
+              {coin.price_change_percentage_24h >= 0 && (
+                <span className="text-green-500">
+                  {coin.price_change_percentage_24h.toFixed(2)}%
+                </span>
+              )}
+              {coin.price_change_percentage_24h < 0 && (
+                <span className="text-red-700">
+                  {coin.price_change_percentage_24h.toFixed(2)}%
+                </span>
+              )}
             </div>
+            <div className="font-bold text-xl flex gap-2">
+              <span>{symbol}</span>
+              <span>{coin.current_price.toLocaleString(currency)}</span>
+            </div>
+          </div>
         ))}
-    </div>
+      </div>
+      )}
+    </>
   )
 }

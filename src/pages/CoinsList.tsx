934106b8f6f2fdaed8c8bcc4861143c7378fe720
jotapemattos <jpmrc49@gmail.com>
2023-04-04 13:06:28 -0300

    add a loader in api calls

--- src/pages/CoinsList.tsx
+++ src/pages/CoinsList.tsx
@@ -3,68 +3,71 @@ import { useContext, useEffect, useState } from 'react'
 import { CurrencyContext } from '../context/Currency'
 import { CoinList } from '../services/api'
 import { CoinsContext } from '../context/Coins'
+import Loader from '../components/Loader'
 
 const CoinsList = () => {
   const { currency, symbol } = useContext(CurrencyContext)
-  const { coins, setCoins } = useContext(CoinsContext)  
+  const { coins, setCoins } = useContext(CoinsContext)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchCoins = async () => {
+    await axios.get(CoinList(currency))
+      .then((response) => setCoins(response.data))
+      .catch((err) => console.log(err.name))
+    setIsLoading(false)
+  }
 
   useEffect(() => {
-    //axios cancellation token in case the user cancels the request
-    const cancelToken = axios.CancelToken.source() 
-    axios.get(CoinList(currency), {cancelToken: cancelToken.token})
-    .then((response) => setCoins(response.data))
-     .catch((err) => {
-        if (axios.isCancel(err)) {
-            console.log("request cancelled")
-        }
-        else {
-            //handleerror
-        }
-     })
-    
-    return () => {
-        cancelToken.cancel()
-    }
+    fetchCoins()
   }, [currency])
 
   return (
-    <div className='coinsList flex flex-col items-center justify-center pb-20 min-w-screen min-h-full'>
-        <div className='w-5/6 flex flex-col gap-10 h-auto mt-40'>
-            {coins.map((coin) => (
-                <div 
-                    key={coin.symbol} 
-                    className='w-full flex items-center justify-between bg-blue-900/20 rounded-2xl shadow-xl'
-                >
-                    <div className='w-1/3 h-28 p-4 flex items-center justify-start gap-4 text-white'>
-                        <img src={coin.image} alt="coin-image" className='h-full'/>
-                        <p className='text-2xl'>{coin.name}</p>
-                        <div className='text-2xl flex items-center justify-center gap-2'>
-                            <span>{symbol}</span>
-                            <span>{coin.current_price.toLocaleString(currency)}</span>
-                        </div>
-                    </div>
-                    <div className='w-1/3 h-28 p-4 flex items-center justify-center gap-4 text-white'>
-                        <p className='text-2xl'>{coin.symbol.toUpperCase()}</p>
-                        <div className=" flex justify-center gap-4">
-                            {coin.price_change_percentage_24h >= 0 && (
-                                <span className="text-green-500">
-                                    {coin.price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                            {coin.price_change_percentage_24h < 0 && (
-                                <span className="text-red-700">
-                                    {coin.price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                        </div>
-                    </div>
-                    <div className='w-1/3 h-28 p-4 flex items-center justify-end gap-4 text-white'>
-                        <p>#{coin.market_cap_rank}</p>
-                    </div>
-                </div>
-            ))}
+    <>
+      {isLoading && (
+        <div className='w-screen h-screen flex items-center justify-items-center'>
+          <Loader/>
         </div>
-    </div>
+      )}
+      {!isLoading && (
+        <div className='coinsList flex flex-col items-center justify-center pb-20 min-w-screen min-h-full'>
+        <div className='w-5/6 flex flex-col gap-10 h-auto mt-40'>
+          {coins.map((coin) => (
+            <div
+              key={coin.symbol}
+              className='w-full flex items-center justify-between bg-blue-900/20 rounded-2xl shadow-xl'
+            >
+              <div className='w-1/3 h-28 p-4 flex items-center justify-start gap-4 text-white'>
+                <img src={coin.image} alt="coin-image" className='h-full' />
+                <p className='text-2xl'>{coin.name}</p>
+                <div className='text-2xl flex items-center justify-center gap-2'>
+                  <span>{symbol}</span>
+                  <span>{coin.current_price.toLocaleString(currency)}</span>
+                </div>
+              </div>
+              <div className='w-1/3 h-28 p-4 flex items-center justify-center gap-4 text-white'>
+                <p className='text-2xl'>{coin.symbol.toUpperCase()}</p>
+                <div className=" flex justify-center gap-4">
+                  {coin.price_change_percentage_24h >= 0 && (
+                    <span className="text-green-500">
+                      {coin.price_change_percentage_24h.toFixed(2)}%
+                    </span>
+                  )}
+                  {coin.price_change_percentage_24h < 0 && (
+                    <span className="text-red-700">
+                      {coin.price_change_percentage_24h.toFixed(2)}%
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div className='w-1/3 h-28 p-4 flex items-center justify-end gap-4 text-white'>
+                <p>#{coin.market_cap_rank}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      )}
+    </>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,41 +7,41 @@ import { SearchQueryContext } from '../context/SearchQuery'
 
 
 const Home = () => {
-  const { search, setSearch} = useContext(SearchQueryContext)
-  const navigate = useNavigate()
-  const handleSearch = (e: {key: string}) => {
-    if (e.key === 'Enter'){
-        navigate('/search/' + search)
+    const { search, setSearch } = useContext(SearchQueryContext)
+    const navigate = useNavigate()
+    const handleSearch = (e: { key: string }) => {
+        if (e.key === 'Enter') {
+            navigate('/search/' + search)
+        }
+        return
     }
-    return 
-  }
 
-  return (
-    <main className="home flex flex-col items-center justify-center gap-28 pb-20 min-w-screen min-h-full">
-        <div className='w-4/5 h-auto pt-40 flex items-start justify-between'>
-            <span className='w-1/2 flex flex-col gap-16 items-start pt-20 justify-start'>
-                <h1 className='text-5xl text-white font-bold'>This is Crypto Corner</h1>
-                <h2 className='text-3xl text-blue-400 font-bold'>Crypto Corner is your one-stop-shop for all the information you need to stay ahead of the game.</h2>
-                <p className='text-lg text-indigo-300'>We are a comprehensive platform that provides up-to-date information on everything related to cryptocurrency, including news, market updates, analysis of different coins and exchanges. Our expert team of researchers and writers covers all major cryptocurrencies and provides valuable resources for seasoned investors and newcomers alike.</p>
-            </span>
-            <span className='h-auto flex items-center justify-center'>
-                <img src={Coin} alt="bitcoin" className='bitcoin absolute'/>
-                <img src={Platform} alt="platform" className='h-full'/>
-            </span>
-        </div>
-        <div className='w-5/6 h-auto flex flex-col items-center justify-center gap-4 text-indigo-300'>
-            <h1 className='font-bold text-3xl'>Search for a coin</h1>
-            <input 
-                type="text" 
-                placeholder='Ex: Bitcoin, Ethereum, Tether, etc.'
-                className='outline-none w-[700px] bg-white/5 p-2 rounded-full text-white transition-all duration-300 text-lg  shadow-cyan-300'
-                onChange={(e) => setSearch(e.target.value)}
-                onKeyDown={handleSearch}
-            />
-        </div>
-        <TrendingCoins/>
-    </main>
-  )
+    return (
+        <main className="home flex flex-col items-center justify-center gap-28 pb-20 min-w-screen min-h-full">
+            <div className='w-4/5 h-auto pt-40 flex items-start justify-between'>
+                <span className='w-1/2 flex flex-col gap-16 items-start pt-20 justify-start'>
+                    <h1 className='text-5xl text-white font-bold'>This is Crypto Corner</h1>
+                    <h2 className='text-3xl text-blue-400 font-bold'>Crypto Corner is your one-stop-shop for all the information you need to stay ahead of the game.</h2>
+                    <p className='text-lg text-indigo-300'>We are a comprehensive platform that provides up-to-date information on everything related to cryptocurrency, including news, market updates, analysis of different coins and exchanges. Our expert team of researchers and writers covers all major cryptocurrencies and provides valuable resources for seasoned investors and newcomers alike.</p>
+                </span>
+                <span className='h-auto flex items-center justify-center'>
+                    <img src={Coin} alt="bitcoin" className='bitcoin absolute' />
+                    <img src={Platform} alt="platform" className='h-full' />
+                </span>
+            </div>
+            <div className='w-5/6 h-auto flex flex-col items-center justify-center gap-4 text-indigo-300'>
+                <h1 className='font-bold text-3xl'>Search for a coin</h1>
+                <input
+                    type="text"
+                    placeholder='Ex: Bitcoin, Ethereum, Tether, etc.'
+                    className='outline-none w-[700px] bg-white/5 p-2 rounded-full text-white transition-all duration-300 text-lg  shadow-cyan-300'
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleSearch}
+                />
+            </div>
+            <TrendingCoins />
+        </main>
+    )
 }
 
 export default Home

7bccacc665a9f4eb90c1d5ddab8a3c9c7bf16e3b
jotapemattos <jpmrc49@gmail.com>
2023-04-06 19:42:41 -0300

    change styles

--- src/components/GlobalStats/CoinsCards.tsx
+++ src/components/GlobalStats/CoinsCards.tsx
@@ -3,8 +3,10 @@ import { useContext, useEffect, useState } from "react"
 import { CurrencyContext } from "../../context/Currency"
 import { TrendingCoins } from '../../services/api'
 import Loader from "../Loader"
+import { Link } from "react-router-dom"
 
 interface TrendingCoinsTypes {
+  id: string,
   current_price: number,
   image: string,
   name: string,
@@ -38,29 +40,33 @@ export const CoinsCards = () => {
       {!isLoading && (
         <div className="w-full grid grid-cols-5 gap-10 text-white">
         {data.map((coin) => (
-          <div
-            key={coin.symbol}
-            className='w-60 h-auto flex flex-col justify-center items-center gap-4 p-2 bg-gradient-to-br from-white/10 to-white/~5 backdrop-blur-sm rounded-xl drop-shadow-lg border border-white/20'
+          <Link 
+            to={'/coinpage/' + coin.id}
+            key={coin.id}
           >
-            <img src={coin.image} alt="coin-image" className="w-20" />
-            <div className=" flex justify-center gap-4">
-              <span className="text-white">{coin.symbol.toUpperCase()}</span>
-              {coin.price_change_percentage_24h >= 0 && (
-                <span className="text-green-500">
-                  {coin.price_change_percentage_24h.toFixed(2)}%
-                </span>
-              )}
-              {coin.price_change_percentage_24h < 0 && (
-                <span className="text-red-700">
-                  {coin.price_change_percentage_24h.toFixed(2)}%
-                </span>
-              )}
+            <div
+              className='w-60 h-56 flex flex-col justify-center items-center gap-4 p-2 bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-sm rounded-xl shadow-2xl hover:cursor-pointer hover:scale-95 transition-all duration-500'
+            >
+              <img src={coin.image} alt="coin-image" className="w-20" />
+              <div className=" flex justify-center gap-4">
+                <span className="text-white">{coin.symbol.toUpperCase()}</span>
+                {coin.price_change_percentage_24h >= 0 && (
+                  <span className="text-green-500">
+                    {coin.price_change_percentage_24h.toFixed(2)}%
+                  </span>
+                )}
+                {coin.price_change_percentage_24h < 0 && (
+                  <span className="text-red-700">
+                    {coin.price_change_percentage_24h.toFixed(2)}%
+                  </span>
+                )}
+              </div>
+              <div className="font-bold text-xl flex gap-2">
+                <span>{symbol}</span>
+                <span>{coin.current_price.toLocaleString(currency)}</span>
+              </div>
             </div>
-            <div className="font-bold text-xl flex gap-2">
-              <span>{symbol}</span>
-              <span>{coin.current_price.toLocaleString(currency)}</span>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
       )}

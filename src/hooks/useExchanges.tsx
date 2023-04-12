347a5f312f3fc1aaea733cecc3d63384cd608afc
jotapemattos <jpmrc49@gmail.com>
2023-04-12 00:01:25 -0300

    create exchange page

---
+++ src/hooks/useExchanges.tsx
@@ -0,0 +1,62 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { ExchangeList } from "../services/api"
+
+interface ExchangesProps {
+	id: string,
+  name: string,
+  year_established: number,
+  country: string,
+  description: string,
+  url: string,
+  image: string,
+  trust_score_rank: number,
+}
+
+export const useExchange = () => {
+  const [exchanges, setExchanges] = useState(Array<ExchangesProps>)
+  const [pages, setPages] = useState(1)
+  const [pageSelected, setPageSelected] = useState(1)
+
+  const pagesArray = [1, 2, 3, 4, 5, 6]
+
+  const handleClick = (page: number) => {
+    setPageSelected(page)
+    setPages(page)
+    window.scroll(0, 0)
+  }
+
+  const handlePreviousPage = () => {
+    if (!(pageSelected === 1)) {
+      setPages(pages - 1)
+      setPageSelected(pages -1)
+      window.scroll(0, 0)
+    } return 
+  }
+
+  const handleNextPage = () => {
+    if (pageSelected < 6) {
+      setPages(pages + 1)
+      setPageSelected(pages + 1)
+      window.scroll(0, 0)
+    } return 
+  }
+
+	const fetchExchanges = async () => {
+		await axios.get(ExchangeList(pages))
+    .then((response) => setExchanges(response.data))
+	}
+
+	useEffect(() => {
+		fetchExchanges()
+	}, [pages])
+
+  return {
+    exchanges,
+    pagesArray,
+    pageSelected,
+    handleClick,
+    handlePreviousPage,
+    handleNextPage
+  }
+}
--- src/pages/Exchanges.tsx
+++ src/pages/Exchanges.tsx
@@ -1,38 +1,8 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { ExchangeList } from "../services/api"
-
-interface ExchangesProps {
-	id: string,
-  name: string,
-  year_established: number,
-  country: string,
-  description: string,
-  url: string,
-  image: string,
-  trust_score_rank: number,
-}
+import { CaretLeft, CaretRight } from "@phosphor-icons/react"
+import { useExchange } from "../hooks/useExchanges"
 
 const Exchanges = () => {
-	const [exchanges, setExchanges] = useState(Array<ExchangesProps>)
-  const [pages, setPages] = useState(1)
-  const [pageSelected, setPageSelected] = useState(1)
-
-  const pagesArray = [1, 2, 3, 4, 5, 6]
-
-  const handleClick = (page: number) => {
-    setPageSelected(page)
-    setPages(page)
-  }
-
-	const fetchExchanges = async () => {
-		await axios.get(ExchangeList(pages))
-    .then((response) => setExchanges(response.data))
-	}
-
-	useEffect(() => {
-		fetchExchanges()
-	}, [pages])
+	const {exchanges, pagesArray, pageSelected, handleClick, handlePreviousPage, handleNextPage } = useExchange()
 
 	return (
 		<div className="min-w-full min-h-full pt-36 pb-16 flex flex-col gap-16 items-center justify-center">
@@ -47,21 +17,31 @@ const Exchanges = () => {
           >
             <img src={exchange.image} alt="exchange-image" />
             <a href={exchange.url}>{exchange.name}</a>
-            {exchange.url === 'https://r.kraken.com/c/2223866/687155/10583' && (<div><a href='https://r.kraken.com/c/2223866/687155/10583'>Kraken</a></div>)}
+            {exchange.url === 'https://r.kraken.com/c/2223866/687155/10583' && (<h1>Kraken</h1>)}
           </div>
         ))}
       </div>
-      <div className="flex justify-center gap-2">
+      <div className="flex items-center justify-center gap-2">
+        <CaretLeft 
+          size={32} 
+          color="#ffffff" 
+          weight="regular"
+          className={!(pageSelected === 1) ? `hover:cursor-pointer` : `hover:cursor-default`}
+          onClick={handlePreviousPage} 
+        />
         {pagesArray.map((page) => (
-          <>
-            
             <button 
               className={page === pageSelected ? `text-[#020017] font-bold bg-white p-2 rounded-full w-10 h-10 opacity-50` : `text-[#020017] font-bold bg-white p-2 rounded-full w-10 h-10`} 
               onClick={() => handleClick(page)}>{page}
             </button>
-            
-          </>
         ))}
+        <CaretRight 
+          size={32} 
+          color="#ffffff" 
+          weight="regular"
+          className={pageSelected < 6 ? `hover:cursor-pointer` : `hover:cursor-default`}
+          onClick={handleNextPage} 
+        />
       </div>
 		</div>
 	)

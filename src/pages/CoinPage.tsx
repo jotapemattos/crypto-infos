baf3f2d04192ed91c51c60df37099986125cf770
jotapemattos <jpmrc49@gmail.com>
2023-04-19 10:25:18 -0300

    fix a style issue

--- src/pages/CoinPage.tsx
+++ src/pages/CoinPage.tsx
@@ -1,25 +1,34 @@
 import CoinPricesGraph from "../components/CoinPricesGraph"
+import Loader from "../components/Loader"
 import { useCoinPage } from "../hooks/useCoinPage"
 
 const CoinPage = () => {
-  const { currency, symbol, singleCoin, params } = useCoinPage()
+  const { currency, symbol, singleCoin, params, isLoading } = useCoinPage()
 
   return (
-    <div className="coinpage min-w-full min-h-full flex flex-col items-center justify-center pt-44">
-      <section className="w-full h-[700px] px-16 flex">
-        <aside className="w-[500px] p-10 flex flex-col gap-10 text-white items-center justify-between border-r border-white">
-          <img src={singleCoin?.image.large} alt="" width="150px"/>
-          <h2 className="text-4xl">{singleCoin?.name}</h2>
-          <p className="text-justify text-lg">{(singleCoin?.description.en.split(". ")[0])}.</p>
-          <p className="text-start w-full text-lg">Rank: {singleCoin?.market_cap_rank}</p>
-          <p className="text-start w-full text-lg">Current Price: {symbol} {singleCoin?.market_data.current_price[currency].toLocaleString(currency)}</p>
-        </aside>
-        <div className="w-full p-4">
-          <CoinPricesGraph coin={params.coin!} currency={currency}/>
+    <>
+      {isLoading && (
+        <div className="w-screen h-screen flex justify-center items-center">
+        <Loader/>
+      </div>
+      )}
+      {!isLoading && (
+        <div className="coinpage min-w-full min-h-full flex flex-col items-center justify-center pt-24 lg:pt-44">
+          <section className="w-full lg:h-[700px] p-4 md:px-10 lg:px-16 flex flex-col lg:flex-row">
+            {<aside className="lg:w-[500px] lg:p-10 flex flex-col gap-10 text-white items-center justify-between lg:border-r lg:border-white">
+              <img src={singleCoin?.image.large} alt="" width="100px lg:150px"/>
+              <h2 className="text-4xl">{singleCoin?.name}</h2>
+              <p className="text-start text-lg">{(singleCoin?.description.en.split(". ")[0])}.</p>
+              <p className="text-start w-full text-lg">Rank: {singleCoin?.market_cap_rank}</p>
+              <p className="text-start w-full text-lg">Current Price: {symbol} {singleCoin?.market_data.current_price[currency].toLocaleString(currency)}</p>
+            </aside>}
+            {<div className="w-full p-4">
+              <CoinPricesGraph coin={params.coin!} currency={currency}/>
+            </div>}
+          </section>
         </div>
-      </section>
-
-    </div>
+      )}
+    </>
   )
 }
 
--- src/pages/Exchanges.tsx
+++ src/pages/Exchanges.tsx
@@ -21,17 +21,19 @@ const Exchanges = () => {
           <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 items-center justify-items-center">
             {exchanges.map((exchange) => (
               <div
-                className="card w-60 h-56 flex flex-col justify-around items-center gap-4 p-2 text-white bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-sm rounded-xl drop-shadow-lg hover:-translate-y-1 hover:opacity-70 hover:cursor-pointer transition-all duration-300"
+                className="card w-60 h-56 flex flex-col justify-center items-center gap-4 p-2 text-white bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-sm rounded-xl drop-shadow-lg hover:-translate-y-1 hover:opacity-70 hover:cursor-pointer transition-all duration-300"
                 key={exchange.id}
               >
                 <h1 className="absolute inset-0 p-2">#{exchange.trust_score_rank}</h1>
-                <img src={exchange.image} alt="exchange-image" />
-                <a href={exchange.url} className="text-xl text-center">{exchange.name}</a>
-                {exchange.url === 'https://r.kraken.com/c/2223866/687155/10583' && (<h1 className="text-xl text-center">Kraken</h1>)}
-                <p className="flex items-center justify-center gap-2">
-                  <MapPin size={24} color="#ff0000" weight="fill" />
-                  {exchange.country}
-                </p>
+                <div className="flex flex-col justify-around items-center gap-4 p-2 relative">
+                  <img src={exchange.image} alt="exchange-image" className="w-14"/>
+                  <a href={exchange.url} target="_blank" className="text-xl text-center">{exchange.name}</a>
+                  {exchange.url === 'https://r.kraken.com/c/2223866/687155/10583' && (<h1 className="text-xl text-center">Kraken</h1>)}
+                  <p className="flex items-center justify-center gap-2">
+                    <MapPin size={24} color="#ff0000" weight="fill" />
+                    {exchange.country}
+                  </p>
+                </div>
               </div>
             ))}
           </div>
